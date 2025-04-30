b2be23ab2bff9c524f8ab9a7946e175c678e0117
Adrian9901 <139763819+Adrian9901@users.noreply.github.com>
2025-05-01 02:28:54 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,5 @@
 <?php
 function dt_enqueue_styles() {
-    $parenthandle = 'divi-style'; 
-    $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-        array(), // if the parent theme code has a dependency, copy it to here
-        $theme->parent()->get('Version')
-    );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get('Version') 
-    );
+    // Styles are intentionally not enqueued
 }
 add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
